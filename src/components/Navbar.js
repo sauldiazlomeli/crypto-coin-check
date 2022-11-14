5ec020705bb37ba8ab08f17125348516e2b79f6a
Saul Diaz <diaz.saul.lomeli@gmail.com>
2022-11-14 09:19:01 -0800

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {FaCoins} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+import './Navbar.css'
+
+
+const Navbar = () => {
+  return (
+    <Link to='/'>
+        <div className='navbar'>
+            <FaCoins className='icon' />
+            <h1> Crypto Coin <span className='blue'>Check</span></h1>
+
+        </div>
+    </Link>
+  )
+}
+
+export default Navbar
