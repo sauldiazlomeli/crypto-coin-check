8f3222cf78fbcae0e1b48f97b59508b0e9fdc518
Saul Diaz <115510415+sauldiazlomeli@users.noreply.github.com>
2022-11-14 10:05:46 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-<BrowserRouter>
+<Router>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-
-);
+  </Router>,
+  document.getElementById('root'));
